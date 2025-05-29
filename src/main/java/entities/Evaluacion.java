e61523b1e774365439a3f6db6c0ae4de27b33054
Juan Martín Berglavaz <parket453@MacBook-Pro-de-Juan-Ma.local>
2025-05-29 10:13:02 -0300

    Agregué getters, setters y constructores

--- src/main/java/entities/Evaluacion.java
+++ src/main/java/entities/Evaluacion.java
@@ -3,7 +3,37 @@ package entities;
 import java.util.Date;
 
 public class Evaluacion {
-    int idUsuario;
-    int calificacion;
-    Date fecha;
+    private int idUsuario;
+    private int calificacion;
+    private Date fecha;
+
+    public Evaluacion(int idUsuario, int calificacion, Date fecha) {
+        this.idUsuario = idUsuario;
+        this.calificacion = calificacion;
+        this.fecha = fecha;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
--- src/main/java/entities/Pelicula.java
+++ src/main/java/entities/Pelicula.java
@@ -3,12 +3,70 @@ package entities;
 import java.util.Date;
 
 public class Pelicula {
-    int id;
-    String titulo;
-    String idiomaOriginal;
-    String genero;
-    Date fechaDeEstreno;
-    int ingresos;
+    private int id;
+    private String titulo;
+    private String idiomaOriginal;
+    private String genero;
+    private Date fechaDeEstreno;
+    private int ingresos;
 
     // [] evaluaciones
+
+
+    public Pelicula(int id, String titulo, String idiomaOriginal, String genero, Date fechaDeEstreno, int ingresos) {
+        this.id = id;
+        this.titulo = titulo;
+        this.idiomaOriginal = idiomaOriginal;
+        this.genero = genero;
+        this.fechaDeEstreno = fechaDeEstreno;
+        this.ingresos = ingresos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdiomaOriginal() {
+        return idiomaOriginal;
+    }
+
+    public void setIdiomaOriginal(String idiomaOriginal) {
+        this.idiomaOriginal = idiomaOriginal;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Date getFechaDeEstreno() {
+        return fechaDeEstreno;
+    }
+
+    public void setFechaDeEstreno(Date fechaDeEstreno) {
+        this.fechaDeEstreno = fechaDeEstreno;
+    }
+
+    public int getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(int ingresos) {
+        this.ingresos = ingresos;
+    }
 }
