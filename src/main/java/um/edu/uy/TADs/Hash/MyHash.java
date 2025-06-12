bc3c24fb296215c73e01afb24e31fef3819ba4a1
IagoMaSp <iagomasp2005@gmail.com>
2025-06-12 08:45:30 -0300

    Se agrego iterador al TAD de Hash

--- src/main/java/um/edu/uy/TADs/Hash/MyHash.java
+++ src/main/java/um/edu/uy/TADs/Hash/MyHash.java
@@ -2,7 +2,7 @@ package um.edu.uy.TADs.Hash;
 
 import um.edu.uy.Exceptions.ElementAlreadyExist;
 
-public interface MyHash<K,T> {
+public interface MyHash<K,T> extends Iterable<T>{
     void insert(K clave, T data) throws ElementAlreadyExist;
     boolean contains(K clave);
     void delete(K clave);
